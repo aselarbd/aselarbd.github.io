71b3c90feaffd4844f462d67983d504a340095ee
Asela Dasanayaka <aselarbd@gmail.com>
2021-06-16 00:11:20 +0200

    first commit

---
+++ assets/js/mapdata.js
@@ -0,0 +1,159 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "800", //'700' or 'responsive'
+    background_color: "#eceff1",
+    background_transparent: "no",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#008602",
+    state_hover_color: "#005800",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#2c312c",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    DEU1572: {
+      name: "Nordrhein-Westfalen",
+      description: " "
+    },
+    DEU1573: {
+      name: "Baden-Württemberg",
+      description: " "
+    },
+    DEU1574: {
+      name: "Hessen",
+      description: " "
+    },
+    DEU1575: {
+      name: "Bremen",
+      description: " "
+    },
+    DEU1576: {
+      name: "Niedersachsen",
+      description: " "
+    },
+    DEU1577: {
+      name: "Thüringen",
+      description: " "
+    },
+    DEU1578: {
+      name: "Hamburg",
+      description: " "
+    },
+    DEU1579: {
+      name: "Schleswig-Holstein",
+      description: " "
+    },
+    DEU1580: {
+      name: "Rheinland-Pfalz",
+      description: " "
+    },
+    DEU1581: {
+      name: "Saarland",
+      description: " "
+    },
+    DEU1591: {
+      name: "Bayern",
+      description: " "
+    },
+    DEU1599: {
+      name: "Berlin",
+      description: " "
+    },
+    DEU1600: {
+      name: "Sachsen-Anhalt",
+      description: " "
+    },
+    DEU1601: {
+      name: "Sachsen",
+      description: " "
+    },
+    DEU3487: {
+      name: "Brandenburg",
+      description: " "
+    },
+    DEU3488: {
+      name: "Mecklenburg-Vorpommern",
+      description: " "
+    }
+  },
+  locations: {
+    "0": {
+      lat: "52.516667",
+      lng: "13.4",
+      name: "Berlin",
+      description: "20+ user feedback of the advertisement"
+    },
+    "1": {
+      lat: "48.1351",
+      lng: "11.5820",
+      name: "Munich",
+      description: "50+ user feedback of the advertisement"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
